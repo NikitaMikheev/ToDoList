e4abe9f72ef7644f9af58691cfb67e7a6d2d9b6d
NikitaMikheev <nikita.miheev.1999@mail.ru>
2022-12-28 21:47:54 +0300

    add element

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ let listArray = [
     }    
 ]
 
+const list = document.getElementById('list');
 const button = document.querySelector('#buttonID');
 button.addEventListener('click', () => {
     var listName = document.querySelector('#inputName');
     var deadline = document.querySelector('#inputDate');
     listArray.push(listName = {name: listName.value, status: false, date: deadline.value});
     console.log(listArray);
+    
+    let newEl = document.createElement('li')
+    newEl.className = "list__item";
+    list.append(newEl);
 })
