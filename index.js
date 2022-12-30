7c8cd8e8c8c40e2b8d1f0deeaf3b8085a91831f8
NikitaMikheev <nikita.miheev.1999@mail.ru>
2022-12-30 19:40:08 +0300

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let listArray = [
-    firstTask = {
+let listArray = [   /* создание массива внутри которого содержатся объекты (для каждой задачи) */ 
+    firstTask = { 
         id: 0,
         name: "firstTask",
         status: false,
@@ -9,13 +9,19 @@ let listArray = [
 
 const list = document.getElementById('list');
 const button = document.querySelector('#buttonID');
-button.addEventListener('click', () => {
+button.addEventListener('click', () => { /* событие при клике на кнопку "Добавить" */
     var listName = document.querySelector('#inputName');
     var deadline = document.querySelector('#inputDate');
-    listArray.push(listName = {name: listName.value, status: false, date: deadline.value});
-    console.log(listArray);
+    listArray.push(listName = {name: listName.value, status: false, date: deadline.value}); /* пуш в массив нового элемента (объекта с параметрами: id, имя задачи, статус, срок) */
+    console.log(listArray); /* убрать при завершении работы */
     
-    let newEl = document.createElement('li')
-    newEl.className = "list__item";
+    let newEl = document.createElement('li') /* создание нового элемента списка */
+    newEl.className = "list__item"; /* присваиваем имя элементу */
+    newEl.innerHTML = `<div class = "list__item-pos"><div><p class= "list__item-text">${listName.name}</p><p>${String(listName.date)}</p></div><div><button>Редактировать</button><button>Удалить</button></div></div>`; /* помещаем текстовое значение для вывода - имя + дата + 2 кнопки*/
     list.append(newEl);
+})
+
+const textName = document.querySelector('class__item-text')   /* подумать */
+textName = document.addEventListener('click', () => {
+    
 })
